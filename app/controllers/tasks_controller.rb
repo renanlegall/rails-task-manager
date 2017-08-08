452ba2ee2a1c39c2062fbee0d673ed324ced74c6
Renan Le Gall <renan.legall@ieseg.fr>
2017-08-08 13:15:24 +0200

    Task Manager done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :destroy, :update]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path(@task)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+end
--- db/migrate/20170808094112_add_description_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToTasks < ActiveRecord::Migration[5.0]
-  def change
-    add_column :tasks, :description, :string
-  end
-end
